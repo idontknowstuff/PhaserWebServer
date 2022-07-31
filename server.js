eefd248992a5778003db3a5fdbfac603ec735090
idontknowstuff <zurizheng88@gmail.com>
2022-07-30 23:22:05 -0600

    added success message

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.get("/", () => {  
+  console.log("Success!");  
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http:/` + PORT);
 });
