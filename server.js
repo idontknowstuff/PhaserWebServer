a9b0b30e8a260e691c29c9d9b7fecbe793f5a844
idontknowstuff <zurizheng88@gmail.com>
2022-12-07 00:34:35 -0700

    added cookies and username in mainpage

--- server.js
+++ server.js
@@ -1,26 +1,56 @@
 const path = require('path');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const DIST_DIR = path.join(__dirname, '/dist');
-const HTML_GAME_FILE = path.join(DIST_DIR, 'index.html');
+const HTML_MAIN_FILE = path.join(DIST_DIR, 'index.html');
 const HTML_LOGIN_FILE = path.join(DIST_DIR, 'login.html');
-const HTML_MAIN_PAGE = path.join(DIST_DIR, 'titlepage.html')
+const HTML_GAME_FILE = path.join(DIST_DIR, 'game.html');
 
+const hardcodedusers = [
+  {email: "user@one.com", password: "abc"},
+  {email: "user@two.com", password: "def"}
+];
+
+app.use(cookieParser());
 app.use(express.static(DIST_DIR));
+app.use(express.urlencoded());
+
+function validateCookie(req, res, next) {
+  const { cookies } = req;
+  if ('email' in cookies) {
+    console.log('email Exists');
+    if (cookies.email != '') {
+      next();
+    }
+    else {
+      res.status(403).send({msg: 'Not Authenticated'});
+    }
+  }
+  else {
+    res.status(403).send({msg: 'Not Authenticated'});
+  }
+}
 
 app.get('/', (req, res) => {
-  res.sendFile(HTML_MAIN_PAGE);
+  res.sendFile(HTML_MAIN_FILE);
 })
 
 app.get('/login', (req, res) => {
   res.sendFile(HTML_LOGIN_FILE);
 });
 
-app.get('/game', (req, res) => {
+app.get('/game', validateCookie, (req, res) => {
   res.sendFile(HTML_GAME_FILE);
 });
 
+// makes cookies 
+app.post('/login', (req, res) => {
+  res.cookie('email', req.body.email);
+  res.cookie('password', req.body.password);
+  res.redirect('/game');
+});
 
 const PORT = process.env.PORT || 8080;
 
--- src/scenes/mainscreen.js
+++ src/scenes/mainscreen.js
@@ -6,66 +6,20 @@ class mainscreen extends Phaser.Scene {
     }
 
     create(){
+        // Background stuff
         this.background = this.add.image(0, 0, "background");
         this.background.setOrigin(0,0);
-        // no login information yet
         this.add.image(500, 150, "title");
-
-
+        
+        // Go Button
         var GoBtn = this.add.image(700,400, "go");
         GoBtn.setInteractive({useHandCursor: true});
         GoBtn.on('pointerdown', () => this.clickGoButton());
 
-
-        // var text = this.add.text(10, 10, 'Please login to play', { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
-
-        // var element = this.add.dom(400, 600).getcache;
-
-        // element.setPerspective(800);
-
-        // element.addListener('click');
-
-        // element.on('click', function (event) {
-
-        // if (event.target.name === 'loginButton')
-        // {
-        //     var inputUsername = this.getChildByName('username');
-        //     var inputPassword = this.getChildByName('password');
-
-        //     //  Have they entered anything?
-        //     if (inputUsername.value !== '' && inputPassword.value !== '')
-        //     {
-        //         //  Turn off the click events
-        //         this.removeListener('click');
-
-    //             //  Tween the login form out
-    //             this.scene.tweens.add({ targets: element.rotate3d, x: 1, w: 90, duration: 3000, ease: 'Power3' });
-
-    //             this.scene.tweens.add({ targets: element, scaleX: 2, scaleY: 2, y: 700, duration: 3000, ease: 'Power3',
-    //                 onComplete: function ()
-    //                 {
-    //                     element.setVisible(false);
-    //                 }
-    //             });
-
-                //  Populate the text with whatever they typed in as the username!
-                // text.setText('Welcome ' + inputUsername.value);
-    //         }
-    //         else
-    //         {
-    //             //  Flash the prompt
-    //             this.scene.tweens.add({ targets: text, alpha: 0.1, duration: 200, ease: 'Power3', yoyo: true });
-    //         }
-    //     }
-
-    // });
- 
-    // this.tweens.add({
-    //     targets: element,
-    //     y: 300,
-    //     duration: 3000,
-    //     ease: 'Power3'
-    // });
+        // Show Email/Username
+        var text = this.add.text(700, 25, 'USER:');
+        let user = this.getCookie('email');
+        var Username = this.add.text(750, 25, user);
     }
 
     update(){
@@ -76,6 +30,22 @@ class mainscreen extends Phaser.Scene {
         // input.manager.resetCursor({ cursor: true });
         this.scene.switch('chooseGame');
     }
+
+    getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+      }
 
 
 
